5f22efbe36b8e73aaa5f83faf59a870d9f26cdc8
ioshe <sdcard11291@gmail.com>
2024-12-17 17:07:29 +0900

    [fix] repair cors error *

--- src/main.py
+++ src/main.py
@@ -17,13 +17,14 @@ logger = logging.getLogger("app")
 
 # 허용할 도메인 (origin), credentials 여부, 허용 메서드, 헤더 설정
 origins = [
-    "http://localhost:3000",  # 프론트엔드 개발 서버 도메인
-    "https://example.com",    # 실제 배포된 도메인
+    "*"
+    # "http://localhost:3000",  # 프론트엔드 개발 서버 도메인
+    # "https://example.com",    # 실제 배포된 도메인
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,       # 허용할 출처 목록
+    allow_origins=[origins],       # 허용할 출처 목록
     allow_credentials=True,      # 쿠키 등 인증정보 허용 여부
     allow_methods=["*"],         # 허용할 HTTP 메서드 (GET, POST 등)
     allow_headers=["*"],         # 허용할 HTTP 헤더
